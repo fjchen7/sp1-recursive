fade069dd3af3770da84e48e0300c80f4f6f461d
jimzk <jimzkx@gmail.com>
2024-02-27 18:25:13 +0800

    Add recursive examples

--- program/src/main.rs
+++ program/src/main.rs
@@ -3,17 +3,23 @@
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
+use sp1_core::{utils::BabyBearBlake3, SP1ProofWithIO, SP1Prover, SP1Stdin, SP1Verifier};
+const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf-plain");
+
 pub fn main() {
     let n = sp1_zkvm::io::read::<u32>();
-    let mut a = 0;
-    let mut b = 1;
-    let mut sum;
-    for _ in 1..n {
-        sum = a + b;
-        a = b;
-        b = sum;
-    }
+    let ret = {
+        let mut stdin = SP1Stdin::new();
+        stdin.write(&n);
+        let mut proof: SP1ProofWithIO<BabyBearBlake3> = {
+            let path = "../../script/proof-with-io_plain.json";
+            let file = std::fs::File::open(path).unwrap();
+            let reader = std::io::BufReader::new(file);
+            serde_json::from_reader(reader).unwrap()
+        };
 
-    sp1_zkvm::io::write(&a);
-    sp1_zkvm::io::write(&b);
+        // Verify proof.
+        SP1Verifier::verify(ELF, &proof).is_ok()
+    };
+    sp1_zkvm::io::write(&ret);
 }
--- script/src/main-plain.rs
+++ script/src/main-plain.rs
@@ -1,28 +1,29 @@
 //! A simple script to generate and verify the proof of a given program.
 
-use std::time::Instant;
-
 use sp1_core::{SP1Prover, SP1Stdin, SP1Verifier};
 
-const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
+const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf-plain");
 
 fn main() {
     // Generate proof.
     let mut stdin = SP1Stdin::new();
     let n = 500u32;
     stdin.write(&n);
-    let start = Instant::now();
     let mut proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
-    println!("Proof Time: {:?}", start.elapsed());
 
     // Read output.
-    // let ret = proof.stdout.read::<bool>();
-    // println!("ret: {}", ret);
+    let a = proof.stdout.read::<u32>();
+    let b = proof.stdout.read::<u32>();
+    println!("a: {}", a);
+    println!("b: {}", b);
 
     // Verify proof.
-    let start = Instant::now();
     SP1Verifier::verify(ELF, &proof).expect("verification failed");
-    println!("Proof Time: {:?}", start.elapsed());
+
+    // Save proof.
+    proof
+        .save("proof-with-io.json")
+        .expect("saving proof failed");
 
     println!("succesfully generated and verified proof for the program!")
 }
